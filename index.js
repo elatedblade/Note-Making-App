f179044174f539a0a13e6087986caab878cf3121
Parth Sharma <parthsharma.elatedblade@gmail.com>
2022-10-04 01:27:34 +0530

    Update JS

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-// Testing
-let a = document.getElementById("addBtn");
-console.log(a);
-console.log("test:\n");
-console.log("working GG");
+function addNote(){
+    let note = document.createElement("div");
+    note.innerText = "NEW NOTE";
+    note.setAttribute("class", "note");
+    menu.appendChild(note);
+    console.log(menu);
+    console.log(note);
+    console.log("Everything worked just fine");
+  };
+  
+  
+  let menu = document.getElementById("notes-menu");
+  let addBtn = document.getElementById("addBtn");
+  addBtn.addEventListener("click", addNote);
+  
