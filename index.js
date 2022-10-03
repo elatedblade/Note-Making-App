fe74003d7d7940f295097dfc2d1e47632011f1e6
Parth Sharma <parthsharma.elatedblade@gmail.com>
2022-10-03 12:33:09 +0530

    Add README.txt

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// function addNote() {
-//     var note = document.createElement("div");
-//     note.setAttribute("class", "note");
-//     console.log(note.getAttribute());
-// }
+function addNote() {
+    var note = document.createElement("div");
+    note.setAttribute("class", "note");
+    console.log(note.getAttribute());
+}
 
 
-// const addBtn = document.getElementById("add-btn").getAttribute;
-// addBtn.addEventListener("click", (e) => addNote);
+const addBtn = document.getElementById("add-btn").getAttribute;
+addBtn.addEventListener("click", (e) => addNote);
 
