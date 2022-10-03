ca965c90b9e30152ada77d2ac73a7db1d093cc3e
Parth Sharma <parthsharma.elatedblade@gmail.com>
2022-10-03 18:56:58 +0530

    changed something(error)

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function addNote() {
-    var note = document.createElement("div");
-    note.setAttribute("class", "note");
-    console.log(note.getAttribute());
-}
-
-
-const addBtn = document.getElementById("add-btn").getAttribute;
-addBtn.addEventListener("click", (e) => addNote);
-
+let addBtn = document.getElementById("add-btn");
