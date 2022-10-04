8fac4ffbe4505e6c9cb4b328404b30b4cd101975
Parth Sharma <parthsharma.elatedblade@gmail.com>
2022-10-05 01:08:18 +0530

    Change in index.js(fix)

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+console.log("Js file has been linked successfully!!!!!");
+
+
+// const menu = document.querySelector("#notes-menu");
+// const addBtn = document.querySelector("#addBtn");
+// or i can use 
+const menu = document.getElementById("notes-menu");
+const addBtn = document.getElementById("addBtn");
+
 function addNote(){
     let note = document.createElement("div");
-    note.innerText = "NEW NOTE";
+    note.textContent = "NEW NOTE";
     note.setAttribute("class", "note");
     menu.appendChild(note);
     console.log(menu);
     console.log(note);
     console.log("Everything worked just fine");
-  };
-  
-  
-  let menu = document.getElementById("notes-menu");
-  let addBtn = document.getElementById("addBtn");
-  addBtn.addEventListener("click", addNote);
-  
+};
+
+addBtn.addEventListener("click", addNote);
