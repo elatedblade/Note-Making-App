9b78cf9c5abf3a0e74ba2d440ff3b309af544ae5
Parth Sharma <parthsharma.elatedblade@gmail.com>
2022-10-06 21:39:38 +0530

    Complete addNote function

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 console.log("Js file has been linked successfully!!!!!");
 
-
-// const menu = document.querySelector("#notes-menu");
-// const addBtn = document.querySelector("#addBtn");
-// or i can use 
-const menu = document.getElementById("notes-menu");
-const addBtn = document.getElementById("addBtn");
-
 function addNote(){
+    const menu = document.getElementById("notes-menu");
     let note = document.createElement("div");
     note.textContent = "NEW NOTE";
     note.setAttribute("class", "note");
     menu.appendChild(note);
-    console.log(menu);
-    console.log(note);
-    console.log("Everything worked just fine");
+    console.log("Note created!");
 };
-
-addBtn.addEventListener("click", addNote);
