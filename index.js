74db7eaa66ccf4dbe0ef4cf007dbd0341383f609
Parth Sharma <parthsharma.elatedblade@gmail.com>
2022-10-28 00:01:23 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 console.log("Js file has been linked successfully!!!!!");
 
 function addNote(){
-    const menu = document.getElementById("notes-menu");
+    var menu = document.getElementById("notes-menu");
     let note = document.createElement("div");
-    note.textContent = "NEW NOTE";
-    note.setAttribute("class", "note");
-    menu.appendChild(note);
-    console.log("Note created!");
+    let text = window.prompt("THE NOTE:");
+    if (text.length != 0) {
+        console.log(text);
+        note.textContent = text;
+        note.setAttribute("class", "note");
+        menu.appendChild(note);
+        console.log("Note created!");
+    }
 };
